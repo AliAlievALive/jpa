1e049738984af9d02b8e51de366c08bbf79efd48
AliAlievALive <madarat@mail.ru>
2022-04-09 19:00:23 +0300

    NamedQuery

--- src/main/java/com/halal/JpaApplication.java
+++ src/main/java/com/halal/JpaApplication.java
@@ -4,12 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 @EnableCaching
 @SpringBootApplication
 public class JpaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(JpaApplication.class, args);
+        final EntityManagerFactory emf = Persistence.createEntityManagerFactory("default");
+
     }
+//    public static void main(String[] args) {
+//        SpringApplication.run(JpaApplication.class, args);
+//    }
 
 }
--- src/main/java/com/halal/entity/SmartphoneEntity.java
+++ src/main/java/com/halal/entity/SmartphoneEntity.java
@@ -8,14 +8,18 @@ import lombok.ToString;
 import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import javax.persistence.NamedQuery;
 
 @Entity
 @NoArgsConstructor
 @ToString
 @Getter
 @Setter
+@NamedQuery(name = SmartphoneEntity.QUERY_GET_ALL, query = "SELECT e FROM SmartphoneEntity e")
 @DiscriminatorValue("smartphone")
 public class SmartphoneEntity extends ProductEntity {
+    public static final String QUERY_GET_ALL = "SmartphoneEntity.getAll";
+
     @Column(columnDefinition = "TEXT")
     private String manufacturer;
 
